9f459f26adb84ffdda86533318ec4eb59f4eb514
jesopo <github@lolnerd.net>
2019-11-27 11:17:05 +0000

    update check_urls.py to use command.regex

--- check_urls.py
+++ check_urls.py
@@ -15,23 +15,27 @@ RE_URL = re.compile(r"https?://\S+", re.I)
 @utils.export("channelset", utils.BoolSetting("check-urls-kick",
     "Enable/Disable automatically kicking users that send malicious URLs"))
 class Module(ModuleManager.BaseModule):
-    @utils.hook("received.message.channel")
+    _name = "CheckURL"
+
+    @utils.hook("command.regex")
+    @utils.kwarg("ignore_action", False)
+    @utils.kwarg("command", "check-url")
+    @utils.kwarg("pattern", utils.http.REGEX_URL)
     def message(self, event):
-        match = RE_URL.search(event["message"])
-        if match and event["channel"].get_setting("check-urls",
+        if event["target"].get_setting("check-urls",
                 event["server"].get_setting("check-urls", False)):
-            url = match.group(0)
+            event.eat()
+
+            url = utils.http.url_sanitise(event["match"].group(0))
             page = utils.http.request(URL_VIRUSTOTAL, get_params={
                 "apikey": self.bot.config["virustotal-api-key"], "resource": url
                 }).json()
 
             if page and page.get("positives", 0) > 1:
-                if event["channel"].get_setting("check-urls-kick", False):
-                    event["channel"].send_kick(event["user"].nickname,
+                if event["target"].get_setting("check-urls-kick", False):
+                    event["target"].send_kick(event["user"].nickname,
                         "Don't send malicious URLs!")
                 else:
-                    self.events.on("send.stdout").call(
-                        module_name="CheckURL", target=event["channel"],
-                        message="%s just send a malicous URL!" %
-                        event["user"].nickname, server=event["server"])
+                    event["stdout"].write("%s just send a malicous URL!" %
+                        event["user"].nickname)
 
